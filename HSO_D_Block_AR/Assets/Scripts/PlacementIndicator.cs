040a7a59df49ff569ca1d8268442e2249defd16b
ben.schnitzer <ben.schnitzer@calren.de>
2021-04-22 17:35:00 +0200

    bugfix stop object's position and rotation from being updated after object has been instantiated implementation of scale and y rotation via touch twist and pinch

--- HSO_D_Block_AR/Assets/Scripts/PlacementIndicator.cs
+++ HSO_D_Block_AR/Assets/Scripts/PlacementIndicator.cs
@@ -32,7 +32,7 @@ public class PlacementIndicator : MonoBehaviour
         rayManager.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), hits, TrackableType.Planes);
 
         // if you hit an AR plane, update the position and rotation
-        if (hits.Count > 0)
+        if (hits.Count > 0 && visualIndicator.activeInHierarchy)
         {
             transform.position = hits[0].pose.position;
             transform.rotation = hits[0].pose.rotation;
