25ac40fb9b81c4a755cb39857df3e2356c352ff7
Matthias Schott <matthias.schott@gmail.com>
2021-05-03 12:39:28 +0200

    Testing events/actions via UIManager

--- HSO_D_Block_AR/Assets/Scripts/UIManager.cs
+++ HSO_D_Block_AR/Assets/Scripts/UIManager.cs
@@ -1,8 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    // private static GameObject visualIndicator;
+    public event Action OnClicked;
 
+    // Method attached to Reset Button in Canvas
+    public void ResetAR()
+    {
+        OnClicked?.Invoke();
+    }
 }
